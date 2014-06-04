d6fac5294a287f9819277bc8ecf15929cf29ed9b
Doug Clinton <doug@wiredthing.com>
2014-06-04 10:04:57 +0100

    Eliminated the actor. ancestry, json and image files are now written to the cache as they’re being streamed from docker.io to the caller.

--- app/controllers/Images.scala
+++ app/controllers/Images.scala
@@ -5,55 +5,50 @@ import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.WS
 import models.ImageId
-import services._
-import play.api.libs.json.{Json, JsError}
 import play.api.Logger
-import system.{Registry, Configuration, AppSystem}
+import system.Registry
 import scala.concurrent.Future
-import services.NotFoundResult
-import play.api.libs.json.JsSuccess
-import services.JsonResult
-import actors.CacheImage
 import play.api.libs.json.JsString
 import scala.Some
+import play.api.libs.iteratee.Enumerator
+import java.io.FileOutputStream
 
 
 object Images extends Controller {
   def ancestry(imageId: ImageId) = Action.async { implicit request =>
-    RegistryService.ancestry(imageId).map {
-      case JsonResult(JsError(errs), _) => BadGateway(errs.toString())
-      case JsonResult(JsSuccess(images, _), headers) => {
-        images.foreach { imageId =>
-          AppSystem.layerActor ! CacheImage(imageId)
-        }
-        Ok(Json.toJson(images.map(_.id))).withHeaders(headers: _*)
-      }
-      case NotFoundResult() => NotFound
-      case ErrorResult(code) => Status(code)
+    Registry.findLocalSource(imageId, Some("ancestry")) match {
+      case Some(localSource) =>
+        Logger.info(s"Supplying ancestry from ${localSource.source}")
+        Future(Ok.sendFile(localSource.file).withHeaders(("Content-Type", "application/json"), ("Content-Length", localSource.length().toString)))
+
+      case None =>
+        respondFromUrl(s"${imageId.id}.ancestry", s"http://registry-1.docker.io/v1/images/${imageId.id}/ancestry")
     }
   }
 
   def json(imageId: ImageId) = Action.async { implicit request =>
-    RegistryService.json(imageId).map {
-      case JsonResult(JsError(errs), _) => BadGateway(errs.toString())
-      case JsonResult(JsSuccess(layerDescriptor, _), headers) => Ok(layerDescriptor.layerJson).withHeaders(headers: _*)
-      case NotFoundResult() => NotFound
-      case ErrorResult(code) => Status(code)
+    Registry.findLocalSource(imageId, Some("json")) match {
+      case Some(localSource) =>
+        Logger.info(s"Supplying json from ${localSource.source}")
+        Future(Ok.sendFile(localSource.file).withHeaders(("Content-Type", "application/json"), ("Content-Length", localSource.length().toString)))
+
+      case None =>
+        respondFromUrl(s"${imageId.id}.json", s"http://registry-1.docker.io/v1/images/${imageId.id}/json")
     }
   }
 
-  def putJson(imageId: ImageId) = Action(parse.file(Registry.buildRegistryPath(s"${imageId.id}.json").mkdirs.file)) { request =>
+  def putJson(imageId: ImageId) = Action(parse.file(Registry.buildRegistryPath(s"${imageId.id}.json").file)) { request =>
     Logger.info(s"Layer json pushed to ${request.body.getAbsolutePath}")
     Ok(JsString(""))
   }
 
-  def putLayer(imageId: ImageId) = Action(parse.file(Registry.buildRegistryPath(imageId.id).mkdirs.file)) { request =>
+  def putLayer(imageId: ImageId) = Action(parse.file(Registry.buildRegistryPath(imageId.id).file)) { request =>
     Logger.info(s"Layer pushed to ${request.body.getAbsolutePath}")
     Ok(JsString(""))
   }
 
 
-  def putChecksum(imageId: ImageId) = Action(parse.file(Registry.buildRegistryPath(s"${imageId.id}.checksum").mkdirs.file)) { request =>
+  def putChecksum(imageId: ImageId) = Action(parse.file(Registry.buildRegistryPath(s"${imageId.id}.checksum").file)) { request =>
     Logger.info(s"Checksum pushed to ${request.body.getAbsolutePath}")
     Ok(JsString(""))
   }
@@ -61,16 +56,15 @@ object Images extends Controller {
   def layer(imageId: ImageId) = Action.async { implicit request =>
     Registry.findLocalSource(imageId) match {
       case Some(localSource) =>
-        Logger.info(s"Supplying image ${imageId.id} from ${localSource.source}")
+        Logger.info(s"Supplying image from ${localSource.source}")
         Future(Ok.sendFile(localSource.file).withHeaders(("Content-Type", "binary/octet-stream"), ("Content-Length", localSource.length().toString)))
 
       case None =>
-        AppSystem.layerActor ! CacheImage(imageId)
-        respondFromUrl(s"http://registry-1.docker.io/v1/images/${imageId.id}/layer")
+        respondFromUrl(imageId.id, s"http://registry-1.docker.io/v1/images/${imageId.id}/layer")
     }
   }
 
-  def respondFromUrl(url: String): Future[Result] = {
+  def respondFromUrl(cacheFileName:String, url: String): Future[Result] = {
     WS.url(url).getStream().map {
       case (response, body) =>
 
@@ -78,14 +72,24 @@ object Images extends Controller {
         if (response.status == 200) {
           // Get the content type
           val contentType = response.headers.get("Content-Type").flatMap(_.headOption)
-            .getOrElse("application/octet-stream")
+            .getOrElse("binary/octet-stream")
+
+          val cacheFile = Registry.buildCachePath(cacheFileName)
+          val os = new FileOutputStream(cacheFile.file)
+
+          val fileWriter :Enumerator[Array[Byte]] = body.map { bytes =>
+            os.write(bytes)
+            bytes
+          }
+
+          fileWriter.onDoneEnumerating(os.close())
 
           // If there's a content length, send that, otherwise return the body chunked
           response.headers.get("Content-Length") match {
             case Some(Seq(length)) =>
-              Ok.feed(body).as(contentType).withHeaders("Content-Length" -> length)
+              Ok.feed(fileWriter).as(contentType).withHeaders("Content-Length" -> length)
             case _ =>
-              Ok.chunked(body).as(contentType)
+              Ok.chunked(fileWriter).as(contentType)
           }
         } else {
           BadGateway
